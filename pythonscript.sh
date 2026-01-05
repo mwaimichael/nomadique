8a2ea3cd72585dd28ff3e22984086ee5f2d90607
mwaimichael <michael.k.mwai@gmail.com>
2026-01-05 16:10:54 +0300

    Update python script

--- pythonscript.sh
+++ pythonscript.sh
@@ -11,20 +11,22 @@ if version == "3.13.5":
 else:
     print('Other version')
 
-os.makedirs("~/Desktop/learning/git/nomad_travel/signage/neon/")
+# os.makedirs("~/Desktop/learning/git/nomad_travel/signage/neon/")
 
 with open("~/Desktop/learning/git/nomad_travel/unique.txt", "w+") as file:
     file.write("This is your unique identifier")
     file.write("azertyqwerty")
 
-#os.mkdir("~/Desktop/learning/git/nomad_travel/signage/neon2")
+# os.mkdir("~/Desktop/learning/git/nomad_travel/signage/neon2")
 
 base, extension = os.path.splitext("~/Desktop/learning/git/nomad_travel/unique.txt")
 
 print(f"base= {base} \n extension= {extension}")
 
-#os.rename("~/Desktop/learning/git/nomad_travel/man_pages","manual")
+# os.rename("~/Desktop/learning/git/nomad_travel/man_pages","manual")
 
-print(sys.getsizeof(base))
-print(sys.getsizeof(rename)) 
+# print(sys.getsizeof(base))
+# print(sys.getsizeof(rename)) 
 
+with open("bonus.txt", "w") as bonus:
+	bonus.write("For large, impactful African datasets, focus on platforms like Opendataforafrica.org, openAFRICA, and Zindi for context-specific data <health, agriculture, finance>, plus Datum Africa and specialized portals like the Africa-geoportal <geospatial> and African language data repositories <e.g., for NER>. Look for data from the African Development Bank, World Bank, and sensors.AFRICA for broad economic, environmental, and real-time data.") 
